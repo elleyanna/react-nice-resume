68c7e9ef991ef354392770c170f5e0ed5ea31391
elleyanna <elleanna.tech@gmail.com>
2020-10-20 20:56:52 -0400

    test

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -81,22 +81,22 @@ class Resume extends Component {
           <div className="nine columns main-col">{work}</div>
         </div>
 
-        <div className="row skill">
-          <div className="three columns header-col">
-            <h1>
-              <span>Skills</span>
-            </h1>
-          </div>
+        // <div className="row skill">
+        //   <div className="three columns header-col">
+        //     <h1>
+        //       <span>Skills</span>
+        //     </h1>
+        //   </div>
 
-          <div className="nine columns main-col">
-            {/* <p>{skillmessage}
-            </p> */}
+        //   <div className="nine columns main-col">
+        //     {/* <p>{skillmessage}
+        //     </p> */}
 
-            <div className="bars">
-              <ul className="skills">{skills}</ul>
-            </div>
-          </div>
-        </div>
+        //     <div className="bars">
+        //       <ul className="skills">{skills}</ul>
+        //     </div>
+        //   </div>
+        // </div>
       </section>
     );
   }
